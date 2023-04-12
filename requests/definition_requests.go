0d62171320642d6dd8a69ff47d249a95da143bff
Yuval <yuvall31@gmail.com>
2023-04-12 21:58:02 +0300

    Add a base function for the requests package

--- requests/definition_requests.go
+++ requests/definition_requests.go
@@ -2,30 +2,15 @@ package requests
 
 import (
 	"io"
-	"io/ioutil"
 	"net/http"
 )
 
 func RerunTask(baseURL string, definitionId string) ([]byte, error) {
-	req, err := http.NewRequest(
+	responseBytes, err := InvokeRequest(
 		http.MethodPost,
 		baseURL+"/api/v1/task/"+definitionId+"/rerun",
 		nil,
 	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Accept", "application/json")
-	response, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	responseBytes, err := ioutil.ReadAll(response.Body)
-
 	if err != nil {
 		return nil, err
 	}
@@ -34,25 +19,11 @@ func RerunTask(baseURL string, definitionId string) ([]byte, error) {
 }
 
 func GetAllTasks(baseURL string) ([]byte, error) {
-	req, err := http.NewRequest(
+	responseBytes, err := InvokeRequest(
 		http.MethodGet,
 		baseURL+"/api/v1/task/",
 		nil,
 	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Accept", "application/json")
-	response, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	responseBytes, err := ioutil.ReadAll(response.Body)
-
 	if err != nil {
 		return nil, err
 	}
@@ -61,7 +32,7 @@ func GetAllTasks(baseURL string) ([]byte, error) {
 }
 
 func DeleteTask(baseURL string, definitionId string) ([]byte, error) {
-	req, err := http.NewRequest(
+	responseBytes, err := InvokeRequest(
 		http.MethodDelete,
 		baseURL+"/api/v1/task/"+definitionId,
 		nil,
@@ -71,24 +42,11 @@ func DeleteTask(baseURL string, definitionId string) ([]byte, error) {
 		return nil, err
 	}
 
-	req.Header.Add("Accept", "application/json")
-	response, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	responseBytes, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
 	return responseBytes, nil
 }
 
 func RunTask(baseURL string, requestBody io.Reader) ([]byte, error) {
-	req, err := http.NewRequest(
+	responseBytes, err := InvokeRequest(
 		http.MethodPost,
 		baseURL+"/api/v1/task",
 		requestBody,
@@ -98,42 +56,16 @@ func RunTask(baseURL string, requestBody io.Reader) ([]byte, error) {
 		return nil, err
 	}
 
-	req.Header.Add("Accept", "application/json")
-	response, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	responseBytes, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
 	return responseBytes, nil
 }
 
 func UpdateTask(baseURL string, requestBody io.Reader) ([]byte, error) {
-	req, err := http.NewRequest(
+	responseBytes, err := InvokeRequest(
 		http.MethodPut,
 		baseURL+"/api/v1/task",
 		requestBody,
 	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Accept", "application/json")
-	response, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	responseBytes, err := ioutil.ReadAll(response.Body)
-
+	
 	if err != nil {
 		return nil, err
 	}
